c1d92b0cbe98965d979b2c621ea70d158a153115
phongnq-ptit <74058326+phongnq-ptit@users.noreply.github.com>
2021-07-01 22:26:51 +0700

    Update C03025.c

--- C03025.c
+++ C03025.c
@@ -1,79 +1,45 @@
 #include<stdio.h>
 #include<math.h>
-#include<string.h>
-#include<stdlib.h>
+#include<stdbool.h>
 
-//int SNT[1000001];
-//void isPrime(){
-//	memset(SNT, 1, sizeof(SNT));
-//	int i,j;
-//	SNT[0] = 0;
-//	SNT[1] = 0;
-//	for(i = 2; i <= sqrt(1000001); i++){
-//		if(SNT[i]){
-//			for(j = i*2; j <= 1000000; j += i){
-//				SNT[j] = 0;
-//			}
-//		}
-//	}
-//}
-
-//int isPrime(int n){
-//	if(n < 2) return 0;
-//	if(n == 2) return 1;
-//	if(n % 2 == 0) return 0;
-//	int i;
-//	for(i = 3; i <= sqrt(n); i += 2){
-//		if(n%i == 0) return 0;
-//	}
-//	return 1;
-//}
-
-int isPrime(int n){
-	if(n <= 1) return 0;
-	if(n <= 3) return 1;
-	if(n%2 == 0 || n%3 == 0) return 0;
-	int i;
-	for(i = 5; i*i <= n; i += 6){
-		if(n%i == 0 || n%(i + 2) == 0) return 0;
-	} 
-	return 1;
+bool Prime(int n){
+    if(n < 2) return 0;
+    if( n == 2) return 1;
+    if(n%2 == 0) return 0;
+    int i;
+    for (i = 3; i <= sqrt(n); i += 2){
+    	if (n % i == 0) return 0;
+	}    
+    return 1;
 }
 
-int tong_SNT(int n){
-	int i, sum = 0;
-	if(isPrime(n) == 0) return 0;
-	while(n != 0){
-		if(n%10 != 2 && n%10 != 3 && n%10 != 7 && n%10 != 5) return 0;
-		sum += n%10;
-		n /= 10;
-	}
-	if(isPrime(sum) == 0) return 0;
-	return 1;
+bool Check(int n){
+    int sum = 0;
+    while (n > 0){
+        int x = n % 10;
+        sum += x;
+        if (x != 2 && x != 3 && x != 5 && x != 7)
+            return 0;
+        n /= 10;
+    }
+    if (Prime(sum)) return 1;
+    return 0;
 }
 
 void bai_lam(){
-	int n,i,a,b, dem = 0;
-	scanf("%d%d", &a, &b);
-	
-	if(a > b){
-		n = a;
-		a = b;
-		b = n;
-	}
-	if(a%2 == 0 && a != 2 && a != 1) a++;
-	for(i = a; i <= b; i += 2){
-		if(tong_SNT(i)) dem++;
-	}
-	printf("%d\n", dem);
+    int l, r, i;
+    scanf("%d%d", &l, &r);
+    int d = 0;
+    for (i = l; i <= r; i++)
+        if (Check(i) && Prime(i)) d++;
+    printf("%d\n", d);
 }
 
-int main(){
-	//isPrime();
-	int a; scanf("%d", &a);
-	while(a--){
-		bai_lam();
+int main()
+{
+    int a; scanf("%d", &a);
+    while (a--){
+    	bai_lam();
 	}
-	return 0;
+    return 0;
 }
-
